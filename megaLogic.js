b4698694a003ee6cb8a2e620f23880dc1b35b92b
Igor Migunov <igormigunov42@gmail.com>
2023-06-02 12:58:43 +0200

    check #1

---
+++ megaLogic.js
@@ -0,0 +1,17 @@
+const f1 = () => {
+  console.log('f1');
+};
+
+const f2 = () => [4, 5, 6];
+
+const getDbValues = async (v = []) => Promise.resolve([...v, 1, 2, 3]);
+
+const f3 = async () => {
+  f1();
+  const d = f2();
+  for (const v of d) {
+    await getDbValues();
+  }
+};
+
+f3();
